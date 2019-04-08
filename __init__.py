758ce720aac7b5963aed731e657ec6da24590123
Huyucheng <jje12345@hotmail.com>
2019-04-08 10:07:35 +0800

    Multi file import

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from AttentionModule.DoubleAttentionModule import DoubleAttention2d
+from AttentionModule.DecoupledAttentionModule import DecoupledAttention2d
+#__all__ = ['DoubleAttentionModule', 'DecoupledAttentionModule']
+# import DoubleAttention2d

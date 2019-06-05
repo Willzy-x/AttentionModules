fe08256e75e8f026f638712d517b9e987c96f277
Huyucheng <jje12345@hotmail.com>
2019-06-05 18:01:32 +0800

    Finish the BAM 2d and 3d

---
+++ BottleneckAttentionModule.py
@@ -0,0 +1,123 @@
+import torch
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Flatten(nn.Module):
+    """
+        Strech a tensor into a 2d tensor with first dimension unchanged.
+    """
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+        
+# 2d version
+class ChannelGate2d(nn.Module):
+    def __init__(self, gate_channel, reduction_ratio=16, num_layers=1):
+        super(ChannelGate2d, self).__init__()
+        self.gate_activation = gate_activation
+        self.gate_c = nn.Sequential()
+        self.gate_c.add_module( 'flatten', Flatten() )
+        gate_channels = [gate_channel]
+        gate_channels += [gate_channel // reduction_ratio] * num_layers
+        gate_channels += [gate_channel]
+        for i in range( len(gate_channels) - 2 ):
+            self.gate_c.add_module( 'gate_c_fc_%d'%i, nn.Linear(gate_channels[i], gate_channels[i+1]) )
+            self.gate_c.add_module( 'gate_c_bn_%d'%(i+1), nn.BatchNorm1d(gate_channels[i+1]) )
+            self.gate_c.add_module( 'gate_c_relu_%d'%(i+1), nn.ReLU() )
+        self.gate_c.add_module( 'gate_c_fc_final', nn.Linear(gate_channels[-2], gate_channels[-1]) )
+
+    def forward(self, in_tensor):
+        avg_pool = F.avg_pool2d( in_tensor, in_tensor.size(2), stride=in_tensor.size(2) )
+        return self.gate_c( avg_pool ).unsqueeze(2).unsqueeze(3).expand_as(in_tensor)
+
+class SpatialGate2d(nn.Module):
+    def __init__(self, gate_channel, reduction_ratio=16, dilation_conv_num=2, dilation_val=4):
+        super(SpatialGate2d, self).__init__()
+        self.gate_s = nn.Sequential()
+        self.gate_s.add_module( 'gate_s_conv_reduce0', nn.Conv2d(gate_channel, gate_channel//reduction_ratio, kernel_size=1))
+        self.gate_s.add_module( 'gate_s_bn_reduce0',	nn.BatchNorm2d(gate_channel//reduction_ratio) )
+        self.gate_s.add_module( 'gate_s_relu_reduce0',nn.ReLU() )
+        for i in range( dilation_conv_num ):
+            self.gate_s.add_module( 'gate_s_conv_di_%d'%i, nn.Conv2d(gate_channel//reduction_ratio, gate_channel//reduction_ratio, kernel_size=3, \
+						padding=dilation_val, dilation=dilation_val) )
+            self.gate_s.add_module( 'gate_s_bn_di_%d'%i, nn.BatchNorm2d(gate_channel//reduction_ratio) )
+            self.gate_s.add_module( 'gate_s_relu_di_%d'%i, nn.ReLU() )
+        self.gate_s.add_module( 'gate_s_conv_final', nn.Conv2d(gate_channel//reduction_ratio, 1, kernel_size=1) )
+
+    def forward(self, in_tensor):
+        return self.gate_s( in_tensor ).expand_as(in_tensor)
+
+class BAM2d(nn.Module):
+    def __init__(self, gate_channel):
+        super(BAM2d, self).__init__()
+        self.channel_att = ChannelGate2d(gate_channel)
+        self.spatial_att = SpatialGate2d(gate_channel)
+    def forward(self,in_tensor):
+        att = 1 + F.sigmoid( self.channel_att(in_tensor) * self.spatial_att(in_tensor) )
+return att * in_tensor
+
+# 3d version
+class ChannelGate3d(nn.Module):
+    """docstring for ChannelGate3d."""
+
+    def __init__(self, gate_channel, reduction_ratio=16, num_layers=1):
+        super(ChannelGate3d, self).__init__()
+        #self.gate_activation = gate_activation
+        self.gate_c = nn.Sequential()
+        self.gate_c.add_module('flatten', Flatten())
+        gate_channels = [gate_channel]
+        gate_channels += [gate_channel // reduction_ratio] * num_layers
+        gate_channels += [gate_channel]
+        # ChannelGate with FC layer, BN, Relu.
+        for i in range(len(gate_channels) - 2):
+            self.gate_c.add_module('gate_c_fc_%d'%i, nn.Linear(gate_channels[i], gate_channels[i+1]))
+            #self.gate_c.add_module('gate_c_bn_%d'%(i+1), nn.BatchNorm1d(gate_channels[i+1]))
+            self.gate_c.add_module('gate_c_relu_%d'%(i+1), nn.ReLU())
+        self.gate_c.add_module('gate_c_fc_final', nn.Linear(gate_channels[-2], gate_channels[-1]))
+
+    def forward(self, in_tensor):
+        avg_pool = F.avg_pool3d(in_tensor, in_tensor.size(2), stride=in_tensor.size(2))
+        # avg_pool: batch, channel, 1, 1, 1
+        print(avg_pool.size())
+        output = (self.gate_c(avg_pool).unsqueeze(2).unsqueeze(3).unsqueeze(4)).expand_as(in_tensor)
+
+        return output
+
+
+class SpatialGate3d(nn.Module):
+    def __init__(self, gate_channel, reduction_ratio=16, dilation_conv_num=2, dilation_val=4):
+        super(SpatialGate3d, self).__init__()
+        self.gate_s = nn.Sequential()
+        self.gate_s.add_module( 'gate_s_conv_reduce0', nn.Conv3d(gate_channel, gate_channel//reduction_ratio, kernel_size=1))
+        self.gate_s.add_module( 'gate_s_bn_reduce0',	nn.BatchNorm3d(gate_channel//reduction_ratio) )
+        self.gate_s.add_module( 'gate_s_relu_reduce0',nn.ReLU() )
+        for i in range( dilation_conv_num ):
+            self.gate_s.add_module( 'gate_s_conv_di_%d'%i, nn.Conv3d(gate_channel//reduction_ratio, gate_channel//reduction_ratio, kernel_size=3, \
+						padding=dilation_val, dilation=dilation_val) )
+            self.gate_s.add_module( 'gate_s_bn_di_%d'%i, nn.BatchNorm3d(gate_channel//reduction_ratio) )
+            self.gate_s.add_module( 'gate_s_relu_di_%d'%i, nn.ReLU() )
+        self.gate_s.add_module( 'gate_s_conv_final', nn.Conv3d(gate_channel//reduction_ratio, 1, kernel_size=1) )
+
+    def forward(self, in_tensor):
+        output = self.gate_s( in_tensor ).expand_as(in_tensor)
+        print(output.size())
+        return output
+
+class BAM3d(nn.Module):
+    """docstring for BAM."""
+
+    def __init__(self, gate_channel):
+        super(BAM3d, self).__init__()
+        self.channel_att = ChannelGate3d(gate_channel)
+        self.spatial_att = SpatialGate3d(gate_channel)
+
+    def forward(self, in_tensor):
+        att = 1 + F.sigmoid(self.channel_att(in_tensor) * self.spatial_att(in_tensor))
+        return att * in_tensor
+
+if __name__ == '__main__':
+    x = torch.randn([2,64, 8,8,8])
+
+    bnet = BAM3d(64)
+    y = bnet(x)
+    print(y.shape)
